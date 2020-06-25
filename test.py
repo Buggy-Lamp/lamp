ab5662760ab58fa255e5b1ff813f2254086676ac
Bart <bart26nl@gmail.com>
2020-06-25 18:15:44 +0200

    forgot some line breaks.

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from utils import writeToLog
 
 def talk_to_lamp():
     bulb_mac_address = config.mac
-    writeToLog("Connecting to bulb at address {}".format(bulb_mac_address))
+    writeToLog(f"Connecting to bulb at address {bulb_mac_address}\n")
     # version is 10 assuming all lamps deployed will be the same
     bulb = MagicBlue(bulb_mac_address, 10)
     try:
@@ -20,21 +20,21 @@ def talk_to_lamp():
 
         if color == "red":
             print("setting lamp to --RED--")
-            writeToLog("setting lamp to --RED--")
+            writeToLog("setting lamp to --RED--\n")
             bulb.set_color([255, 0, 0])
         elif color == "green":
             bulb.set_color([0, 255, 0])
             print("setting lamp to --GREEN--")
-            writeToLog("setting lamp to --GREEN--")
+            writeToLog("setting lamp to --GREEN--\n")
         elif color == "orange":
             bulb.set_color([255, 165, 0])
             print("setting lamp to --ORANGE--")
-            writeToLog("setting lamp to --ORANGE--")
+            writeToLog("setting lamp to --ORANGE--\n")
         else:
             bulb.set_color([0, 0, 255])
             print("INVALID color setting lamp to --BLUE--")
-            writeToLog("INVALID color setting lamp to --BLUE--")
+            writeToLog("INVALID color setting lamp to --BLUE--\n")
     except:
         print("couldn't get data from API setting lamp to --BLUE--")
-        writeToLog("couldn't get data from API setting lamp to --BLUE--")
+        writeToLog("couldn't get data from API setting lamp to --BLUE--\n")
         bulb.set_color([0, 0, 255])
